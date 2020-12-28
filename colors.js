19d2daeeaca7f5ab371cfd73899a1842fbf34d6a
mehot98 <76448240+mehot98@users.noreply.github.com>
2020-12-29 08:08:09 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,30 @@
+function setColor(color){
+  var alist = document.querySelectorAll('a');
+  var i = 0;
+  while(i < alist.length){
+  console.log(alist[i]);
+  alist[i].style.color = color;
+  i = i + 1;
+}
+}
+
+  function nightDayHandler(self){
+      if(self.value==='night'){
+        document.querySelector('body').style.backgroundColor='black';
+        document.querySelector('body').style.color='white';
+        self.value='day';
+
+        setColor('powderblue');
+
+      }
+      else{
+        document.querySelector('body').style.backgroundColor='white';
+        document.querySelector('body').style.color='black';
+        self.value='night';
+
+
+        setColor('red');
+
+      }
+
+      }
