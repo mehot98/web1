c8f0539080ab2c40f8e6bf190dc64531a6418cc1
mehot98 <76448240+mehot98@users.noreply.github.com>
2021-01-05 09:35:17 +0900

    Add files via upload

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -29,13 +29,13 @@ export class Ball{
     const minX = this.radius;
     const maxX = stageWidth - this.radius;
     const minY = this.radius;
-    const maxY = stageHeight - this.radidus;
+    const maxY = stageHeight - this.radius;
 
     if (this.x <= minX || this.x >= maxX) {
       this.vx *= -1;
       this.x += this.vx;
 
-    } else if (this.y <= minX || this.y >= maxX) {
+    } else if (this.y <= minY || this.y >= maxY) {
       this.vy *= -1;
       this.y += this.vy;
 
